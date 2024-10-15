8bd5ae7d6fa708d2af4f55eeb30a8333548eda0e
Bayu Darmawan <40495581+gorillaworkout@users.noreply.github.com>
2024-10-15 14:04:48 +0700

    update

---
+++ src/app/home_jastip/home_jastip.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import cyberpunk_3 from "../../public/cyberpunk/3.png";
+import { Fieldset } from "../catalyst/fieldset";
+import { Textarea } from "../catalyst/textarea";
+import { Input } from "../catalyst/input";
+
+export default function HomeJastip() {
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full bg-red-500">
+        <Input className="bg-blue-500" />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,17 @@ import { Header } from "./components/Header";
 import { Loader } from "./components/Loader";
 import { AudioPlayer } from "./components/AudioPlayer";
 import cyberpunk_3 from '../../public/cyberpunk/3.png';
-import HomePlayer from "./components/HomePlayer";
+import HomeJastip from "./home_jastip/home_jastip";
 
 // import MenuOption from "./component/MenuOption";
 
 export default function Home() {
 
   return (
-    <div className="w-full h-full flex flex-col bg-white">
-      <Loader />
-      {/* <Header /> */}
-      <div className="h-full w-screen z-5 bg-white">
-        <HomePlayer />
+    <div className="w-full h-full flex flex-col bg-white mx-auto max-w-7xl px-6 lg:px-8  py-8 lg:py-10">
+      <div className="h-full w-full z-5">
+        <HomeJastip />
       </div>
-      <AudioPlayer />
     </div>
   );
 }
